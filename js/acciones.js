7e7683a534e7176f7d36536fa97139774bb23903
abram06 <139307440+abram06@users.noreply.github.com>
2026-05-29 00:16:25 -0500

    Agrega funciones JavaScript para mostrar información

---
+++ js/acciones.js
@@ -0,0 +1,11 @@
+function mostrarEdad(){
+    document.getElementById("respuesta").innerText = "🎂 Tengo 21 años";
+}
+
+function mostrarTelefono(){
+    document.getElementById("respuesta").innerText = "📞 Mi número de teléfono es 921949456";
+}
+
+function mostrarCorreo(){
+    document.getElementById("respuesta").innerText = "📧 Mi correo electrónico es abram15castillo@gmail.com";
+}
